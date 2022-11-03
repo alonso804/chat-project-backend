f8af51d522157fce69e58ef6ec4119e39280ebce
alonso804 <alonsobs804@gmail.com>
2022-11-03 16:18:21 -0500

    feat: add chat controller

--- src/controllers/chat/get-chat.controller.ts
+++ src/controllers/chat/get-chat.controller.ts
@@ -12,9 +12,9 @@ export const getChat = async (req: Request, res: Response) => {
   const token = (req.headers.authorization as string).split(' ')[1];
 
   try {
-    const reciever = await UserModel.findOne({ username }).lean();
+    const receiver = await UserModel.findOne({ username }).lean();
 
-    if (!reciever) {
+    if (!receiver) {
       return res.status(404).json({ message: 'User not found' });
     }
 
@@ -33,27 +33,64 @@ export const getChat = async (req: Request, res: Response) => {
 
     const chat = await ChatModel.findOne(
       {
-        users: [decoded.id, reciever._id],
+        users: {
+          $all: [receiver._id, decoded.id],
+        },
       },
       {
-        messages: {
-          content: 1,
-          sender: 1,
-          createdAt: 1,
-        },
+        _id: 1,
       }
-    ).populate('users', 'username -_id');
+    );
 
     if (!chat) {
       return res.status(200).json({
         chat: {
-          users: [{ username: decoded.username }, { username: reciever.username }],
+          receiver: receiver.username,
           messages: [],
         },
       });
     }
 
-    return res.status(200).json({ chat });
+    const sortedChat = await ChatModel.aggregate([
+      {
+        $match: {
+          _id: chat._id,
+        },
+      },
+      {
+        $unwind: '$messages',
+      },
+      {
+        $sort: {
+          'messages.createdAt': -1,
+        },
+      },
+      {
+        $group: {
+          _id: '$_id',
+          messages: {
+            $push: {
+              content: '$messages.content',
+              createdAt: '$messages.createdAt',
+              isSender: {
+                $cond: {
+                  if: {
+                    $eq: ['$messages.sender', decoded.username],
+                  },
+                  then: true,
+                  else: false,
+                },
+              },
+            },
+          },
+          receiver: {
+            $first: receiver.username,
+          },
+        },
+      },
+    ]);
+
+    return res.status(200).json({ chat: sortedChat[0] });
   } catch (error) {
     logger.error(error);
     return res.status(500).json({ message: error });
--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 
+import { verifyToken } from 'middlewares/commons.middlewares';
+
 import { createChat } from 'controllers/chat/create-chat.controller';
 import { getChat } from 'controllers/chat/get-chat.controller';
 import { sendMessage } from 'controllers/chat/send-message.controller';
 
 const authRoutes = Router();
 
-authRoutes.post('/:username', createChat);
+authRoutes.post('/:username', verifyToken, createChat);
 
-authRoutes.put('/:chatId', sendMessage);
+authRoutes.put('/:chatId', verifyToken, sendMessage);
 
-authRoutes.get('/:username', getChat);
+authRoutes.get('/:username', verifyToken, getChat);
 
 export default authRoutes;
